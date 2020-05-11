e7a03fe7024bfcc26c9ff141a8f9722e232a3b11
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:25:52 +0700

    Tugas 17 Upperlower JS
    Ubahlah kalimat berikut menjadi lower case dan upper case :
    
    -Saya beLajar bahaSa peMrograman deNgan khUsuk. (ubah ke lower case dan upper case)
    -Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual. (ubah ke lower case dan upper case)

---
+++ tugas17upperlower.js
@@ -0,0 +1,9 @@
+function upperlower(){
+  var word = 'Saya beLajar bahaSa peMrograman deNgan khUsuk';
+  var word1 = 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual';
+  console.log(word.toLowerCase());
+  console.log(word.toUpperCase());
+  console.log(word1.toLowerCase());
+  console.log(word1.toUpperCase());
+}
+upperlower()
